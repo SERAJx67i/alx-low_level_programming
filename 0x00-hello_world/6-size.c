d35f67ce839859a7a11fba02deda3f162686334f
SERAJx67i <SerajEddinee@gmail.com>
2023-07-14 06:21:00 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 	int main(void)
 {
-	sizeof(char);
-	sizeof(int);
-	sizeof(long int);
-	sizeof(long long int);
-	sizeof(float);
+	printf("Size of a char %d",sizeof(char));
+	printf("Size of an int %c",sizeof(int));
+	printf("Size of a long int %e",sizeof(long int));
+	printf("Size of a long long int %a",sizeof(long long int));
+	printf("Size of a float %b",sizeof(float));
 
 	return (0);
 }
