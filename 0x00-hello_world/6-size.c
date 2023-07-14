5c11fbcaa0d7443a212ef77d87b9682eb6f8181e
SERAJx67i <SerajEddinee@gmail.com>
2023-07-14 06:12:01 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return always 0 when success  
+ */
+	int main(void)
+{	
+	sizeof(char);
+	sizeof(int);
+	sizeof(long int);
+	sizeof(long long int);
+	sizeof(float);
+
+	return (0);
+}
