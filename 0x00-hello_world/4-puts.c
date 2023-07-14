5f35418ee853e4e8d14612363e4c8d042ef34f87
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 18:59:51 -0700

    -_-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 		* Entry point of the program
 		* Return to (0) when success
 		*/
-		puts("\"Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle"\n);
 		return (0);
 }
