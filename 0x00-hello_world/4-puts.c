6b01a8c7058e8b2c01b09147dd2717067094a869
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 19:14:18 -0700

    Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-* Entry point of the program
-* Return to (0) when success
+* program that prints exactlyProgramming is like building a multilingual puzzle, followed by a new line using put function
+* Return to 0 when success
 */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
