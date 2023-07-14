e74f6bc50898d450638bfcef72869c307e048d3e
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 19:02:10 -0700

    Im tires

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 		* Entry point of the program
 		* Return to (0) when success
 		*/
-		puts("\"Programming is like building a multilingual puzzle"\n);
-		return (0);
+		puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
