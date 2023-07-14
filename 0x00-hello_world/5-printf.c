7765298faee83c112089075613b951929d1f72e2
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 19:21:39 -0700

    idl

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * C program
+ * printing string 
+ * Return 0 when success
+ *
+ */
+int main(void)
+{		
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
