5704bc3b133a4cf78efa0ce77d82dc975c10226c
SERAJx67i <SerajEddinee@gmail.com>
2023-07-14 07:27:10 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,13 @@
  *
  * Return: always 0 when success
  */
-	int main(void)
+        int main(void)
 {
-	printf("Size of a char %d",sizeof(char));
-	printf("Size of an int %c",sizeof(int));
-	printf("Size of a long int %e",sizeof(long int));
-	printf("Size of a long long int %a",sizeof(long long int));
-	printf("Size of a float %b",sizeof(float));
+        printf("Size of a char : %d",sizeof(char));
+        printf("Size of an int : %c",sizeof(int));
+        printf("Size of a long int : %e",sizeof(long int));
+        printf("Size of a long long int : %a",sizeof(long long int));
+        printf("Size of a float : %b",sizeof(float));
 
-	return (0);
+        return (0);
 }
