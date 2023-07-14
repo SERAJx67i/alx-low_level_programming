918c034884944a94c6fb2cabe45765019a02585d
SERAJx67i <SerajEddinee@gmail.com>
2023-07-14 07:37:28 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char : %d n/", sizeof(char));
-	printf("Size of an int : %c n/", sizeof(int));
-	printf("Size of a long int : %e n/", sizeof(long int));
-	printf("Size of a long long int : %a n/", sizeof(long long int));
-	printf("Size of a float : %b n/", sizeof(float));
+	printf("Size of a char : %d \n", sizeof(char));
+	printf("Size of an int : %c \n", sizeof(int));
+	printf("Size of a long int : %g  \n", sizeof(long int));
+	printf("Size of a long long int : %a \n", sizeof(long long int));
+	printf("Size of a float : %b \n", sizeof(float));
 
 return (0);
 }
