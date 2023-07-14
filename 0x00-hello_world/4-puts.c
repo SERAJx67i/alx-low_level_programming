7eb6aadffb28664b28a811324185853155cffbe2
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 18:35:06 -0700

    -_-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-                #include<stdio.h>
-
-                int main ()
-            {
+#include<stdio.h>
+		 int main ()
+{         
 		puts("Programming is like building a multilingual puzzle n/");
-		return 0;
-            }
+		return(0);
+}
