4bb3ed5ca35c9879e92c62d345e34f433e1483ad
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 18:50:02 -0700

    -_-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 		 int main(void)
 {		/*Puts is a C function which we can output a string*/
-		puts("\"Programming is like building a multilingual puzzle \n");
+		puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 }
