e9ee4e3be9317f2dba7925953fbf94c792c92871
SERAJx67i <SerajEddinee@gmail.com>
2023-07-13 19:54:44 -0700

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * C program
- * printing string
- * Return 0 when success
- *
- */
+*main - entry point
+*
+*Return:  always 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
