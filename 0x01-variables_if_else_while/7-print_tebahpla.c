d25c4b971a943ecc30ef305a4e70f11c32331647
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 16:06:08 -0700

    CC

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 if false, otherwise if otherwise.
+ */
+int main(void)
+{
+	char a = 123;
+	char b = '\n';
+while (a > 97)
+{
+	a--;
+	putchar(a);
+}
+	putchar(b);
+	return (0);
+}
