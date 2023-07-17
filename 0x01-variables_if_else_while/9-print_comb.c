b9257834996e2593bc20f7db36d1268535538f60
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 16:37:41 -0700

    CD

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0 if false, otherwise if otherwise.
+ */
+int main(void)
+{
+	int a = 47;
+	int b = 10;
+	int c = 44;
+	int s = 32;
+while (a < 57)
+{
+	a++;
+	putchar(a);
+	putchar(c);
+	putchar(s);
+}
+	putchar(b);
+	return (0);
+}
