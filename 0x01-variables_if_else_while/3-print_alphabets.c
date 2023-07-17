3f31779361a0c12b5d2901f26cad7b6c2c01f614
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 13:51:53 -0700

    CC

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@ int main(void)
 	char a = 64;
 	char c = 96;
 	char b = '\n';
+while (c < 122)
+{
+	c++;
+		putchar(c);
+}
 while (a < 90)
 {
 	a++;
 		putchar(a);
 }
-while (c < 122)
-{
-	c++;
-		putchar(c);
 		putchar(b);
-}
 	return (0);
 }
