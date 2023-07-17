484448e695cd86ed1def749e16f8a07ea49dacb1
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 13:41:59 -0700

    CCC

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0 if false, otherwise if otherwise.
+ */
+int main(void)
+{
+	char a = 64;
+	char aa = 96;
+	char b = '\n';
+while (a < 90)
+	a++;
+		putchar(a);
+while (aa < 122)
+	aa++;
+		putchar(aa);
+		putchar(b);
+	return (0);
+}
