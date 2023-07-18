9cd301788c66956b63c5b4c87fa4a5b359ca8dc8
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 18:55:08 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point 
+ * main - entry point
  * Return: 1 always
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is 
+useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
