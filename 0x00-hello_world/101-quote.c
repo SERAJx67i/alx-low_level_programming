505788f5ebb5faa9913ef27896382e9e5ed9c4fa
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 19:11:30 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  * Return: 1 always
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Kor
-par, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
