dcc202ce2a93e2f7eaf1f4a12764822de60be55e
SERAJx67i <SerajEddinee@gmail.com>
2023-07-17 18:39:22 -0700

    C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,15 @@
 int main(void)
 {
 	int n = 97;
-while (n <= 122)	
+	while (n <= 122)
 {
-	if (n == 101 || n == 111)
+		if (n == 101 || n == 113)
 		{
 			n++;
 			continue;
 		}
 	putchar(n);
 	n++;
-
 }
 	putchar('\n');
 	return (0);
