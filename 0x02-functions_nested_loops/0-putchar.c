636fd10ff5dee5a3995d0e959c5dd8c9d210f97c
SERAJx67i <SerajEddinee@gmail.com>
2023-07-22 07:00:04 -0700

    CCC

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <main.h>
+#include "alx-low_level_programming/0x02-functions_nested_loops/main.h"
+#include <unistd.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int c(char *_putchar)
+{
+	
+	return (0);
+}
