58e4e13f20720068aec87c4d45d123f9cbea54cf
SERAJx67i <SerajEddinee@gmail.com>
2023-07-26 10:32:31 -0700

    CF

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - a function that prints the sign of a number.
+ * @n: an integer
+ * Return: 1 if n > 0, 0 if n == 0, -1 if n < 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
+}
