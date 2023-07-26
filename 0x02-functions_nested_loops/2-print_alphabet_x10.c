89a424d0249124b51328789fdabf1fffe35964ec
SERAJx67i <SerajEddinee@gmail.com>
2023-07-26 08:18:17 -0700

    C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Write a function that prints 10 times the alphabet
+ * in lowercase, followed by a new line.
+ * return: 0 always if success
+ */
+void print_alphabet_x10(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
