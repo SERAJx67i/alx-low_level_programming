1bd9f0a6e203c710aa34d2065d377dd3136dd9fc
SERAJx67i <SerajEddinee@gmail.com>
2023-07-26 04:49:08 -0700

    CC

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 IF Success.
+ */
+void print_alphabet(void);
+int main(void)
+{
+	int a = 96;
+	int b = 10;
+	while (a < 122)
+	{
+		a++;
+		putchar(a);
+	}
+		putchar(b);
+		return (0);
+}
