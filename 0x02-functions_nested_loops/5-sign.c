cefe1d269a752c14ced485a102f9b411cef36dbf
SERAJx67i <SerajEddinee@gmail.com>
2023-07-27 01:16:04 -0700

    C

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		/*printf("+1");*/
-		return (1);
+		printf("+");
+		return(1);
 	}
 	else if (n == 0)
 	{
-		/*printf("00");*/
-		return (0);
+		printf("0");
+		return(0);
 	}
 	else
 	{
-		/*printf("-1");*/
-		return (-1);
+		printf("-");
+		return(1);
 	}
 	return (0);
 }
