c8391450a46c37f70379aa290da76e9ad20c7b1c
SERAJx67i <SerajEddinee@gmail.com>
2023-07-30 22:07:18 +0100

    C

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,8 +5,9 @@
  * _abs - a function that computes the absolute value of an integer.
  * Return: 0 always (Success)
  */
-int _abs(int j)
+int _abs(int)
 {
-	abs(j);
+	int r;
+	abs(r);
 	return (0);
 }
