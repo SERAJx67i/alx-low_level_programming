a83a41d61d4504f253327a8ac4aa170376c7c941
SERAJx67i <SerajEddinee@gmail.com>
2023-08-12 15:45:38 +0100

    C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,11 +15,11 @@ void print_times_table(int n)
 			{
 				if (b < 10)
 				{
-				printf(",%-3d", a * b);
+				printf("%0d, ", a * b);
 				}
 				else if (b == 10)
 				{
-					printf("%1d", a * b);
+					printf("%d", a * b);
 				}
 				b++;
 			if (b > 10)
