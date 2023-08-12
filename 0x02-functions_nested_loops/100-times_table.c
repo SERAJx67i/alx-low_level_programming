f762d26b7e3b324a1b337628657cbf8b280f5c11
SERAJx67i <SerajEddinee@gmail.com>
2023-08-12 13:04:11 +0100

    C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,18 +12,18 @@ void print_times_table(int n)
 		int b;
 	while (a <= 10 && b <= n)
 	{
-		if (n <= 15)
+		while (n <= 15)
 		{
-			if (n == 0)
-			{
+				if (n == 0)
+				{
 				printf("0\n");
 				exit(EXIT_FAILURE);
-			}
-		printf("%d, ", a * b);
-		a++;
-		b++;
+				}
+			printf("%d, ", a * b);
+			a++;
+			b++;
 		}
-		else if (n > 15 || n < 0)
+		if (n > 15 || n < 0)
 		{
 			exit(EXIT_FAILURE);
 		}
