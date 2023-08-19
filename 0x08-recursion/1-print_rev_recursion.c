345df4cc6f16179aff2ddcdc9527580480a9a2c8
SERAJx67i <SerajEddinee@gmail.com>
2023-08-19 20:53:39 +0100

    C

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - a function that prints a string in reverse.
+ * @s : a character.
+ * Return: no value.
+ */
+void _print_rev_recursion(char *s)
+{
+	*s = 
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s: character.
+ * Return: an integer.
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
